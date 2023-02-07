3cadced0e480dc65e43fc30c4bd691decdcab300
M0o0o0o <dlandif22@gmail.com>
2023-02-07 16:52:45 +0900

    ini

---
+++ src/main/java/m0o0o0o/jacoco/jacocostudy/JacocoStudyApplication.java
@@ -0,0 +1,13 @@
+package m0o0o0o.jacoco.jacocostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacocoStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/m0o0o0o/jacoco/jacocostudy/JacocoStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package m0o0o0o.jacoco.jacocostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
